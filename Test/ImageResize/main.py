897d64c96f6a6f7216b1695235638bb6753cf4eb
CHIU-YIKAI <andy757588596@gmail.com>
2022-08-17 14:17:43 +0800

    Update resize function

--- Test/ImageResize/main.py
+++ Test/ImageResize/main.py
@@ -1,12 +1,20 @@
 import cv2 
 import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--originDataFolder", type=str, default="./originData/")
+parser.add_argument("--outputDataFolder", type=str, default="./outputData/")
+
+def main(args):
+    fileList = os.listdir(args.originDataFolder)
+    for fileName in fileList:
+        img = cv2.imread(args.originDataFolder + fileName)
+        img = cv2.resize(img, (2048, 1024), interpolation=cv2.INTER_AREA)
+        cv2.imwrite(args.outputDataFolder + fileName, img)
 
 
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(args)
 
-fileList = ["001", "002", "003", "004", "005"]
-fileList = ["006"]
-
-for fileName in fileList:
-    img = cv2.imread("./" + fileName + ".jpeg")
-    img = cv2.resize(img, (2048, 1024), interpolation=cv2.INTER_AREA)
-    cv2.imwrite("./" + fileName + ".jpg", img)
